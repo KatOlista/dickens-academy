444cc7ec0d049320e386b5e8f4ea988a9b0ecd50
KatOlista^^! <olya.katyushina@gmail.com>
2024-11-17 12:39:15 +0100

    fix small style issues

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,28 +26,24 @@ export const Hero = () => {
 
   return (
     <section id='Home' className={styles.hero}>
-      <div className='container'>
-        <div className={styles.hero__content}>
-          <div className={styles.hero__info}>
-            <h1 className={styles.hero__title}>
-              <span>Welcome to &nbsp;</span>
+      <div className={styles.hero__content}>
+        <div className={styles.hero__info}>
+          <h1 className={styles.hero__title}>
+            <span>Welcome to &nbsp;</span>
 
-              Dickens Executive Coaching Academy
-            </h1>
+            Dickens Executive Coaching Academy
+          </h1>
 
-            <p className={styles.hero__description}>
-              Your resource of choice for coaching education
-            </p>
+          <p className={styles.hero__description}>
+            Your resource of choice for coaching education
+          </p>
 
-            <Button buttonClassName={styles.hero__button}>
-              Explore Courses
-            </Button>
-          </div>
-
-          <div className={styles.hero__picture}>
-            <img className={styles.hero__img} src={heroImg} alt="coaching session" />
-          </div>
+          <Button buttonClassName={styles.hero__button}>
+            Explore Courses
+          </Button>
         </div>
+
+        <img className={styles.hero__img} src={heroImg} alt="coaching session" />
       </div>
     </section>
   )
